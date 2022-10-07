a2c8c3212b020e82fc6be3377113398a6913f5f1
unknown <aphiwemambongwe@gmail.com>
2022-10-07 15:42:13 +0200

    version 1

---
+++ springboot_website/src/main/java/com/SpringbootWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebsiteApplication.class, args);
+	System.out.println("server is running"); }
+
+}
